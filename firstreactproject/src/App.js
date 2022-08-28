08e483e85ef3296f069ee94705d63c0bc1d155b7
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-08-28 13:19:41 -0700

    making component directory and starting to make the navbar

--- firstreactproject/src/App.js
+++ firstreactproject/src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-
+import Navbar from "./component/Navbar";
 class App extends Component{
   render(){
     return(
       <div>
-        <p>make sure everythings is good</p>
+        <Navbar />
       </div>
     )
   }
---
+++ firstreactproject/src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from "./Navbar.module.css";
+import logo from "../images/Logo.png"
+const Navbar = () => {
+    return (
+        <header className={styles.header}>
+            <div className= {styles.listContainer}>
+                <ul className={styles.list}>
+                    <li>Home page</li>
+                    <li>Product</li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+            <div className={styles.logo}>
+                <img src={logo} alt="logo" />
+            </div>
+        </header >
+    );
+};
+
+export default Navbar;
