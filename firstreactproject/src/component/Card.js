50afa2c25c32e885ae427fc97e09b530654fcf6c
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-09-05 16:10:06 -0700

    buttoms for countiong for card section are added to the project and they work perfectly

--- firstreactproject/src/component/Card.js
+++ firstreactproject/src/component/Card.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import styles from "./Card.module.css";
+import down from "../images/down-arrow.svg";
+import up from "../images/up-arrow.svg";
+
 
 class Card extends Component {
+    constructor (){
+        super();
+        this.state = {
+            counter: 0,
+        }
+    }
+
+    downHandler = () =>{
+        if (this.state.counter > 0){
+            this.setState(prevState =>({
+                counter: prevState.counter - 1,
+            }))
+        }
+    }
+
+    upHandler = () =>{
+        this.setState(prevState =>({
+            counter: prevState.counter + 1,
+        }))
+    }
+
+
     render() {
         const {image, name, cost} = this.props;
+        const {counter} = this.state;
         return (
             <div className={styles.container}>
                 <img src={image} alt="laptops" />
                 <h3>{name}</h3>
                 <p>{cost}</p>
+                <div className={styles.counter}>
+                    <img className={this.state.counter ? "" : styles.deactive} src={down} alt="Arrow" onClick={this.downHandler} />
+                    <span>{counter}</span>
+                    <img src={up} alt="Arrow" onClick={this.upHandler} />
+
+                </div>
             </div>
         );
     }
