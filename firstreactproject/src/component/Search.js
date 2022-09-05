b4948cadcfc8cd065230539de7f6819ea398f4db
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-09-05 16:27:02 -0700

    add event handeler to the search box

--- firstreactproject/src/component/Search.js
+++ firstreactproject/src/component/Search.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css";
 
 class Search extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            text: "",
+        }
+    }
+
+    changeHandler = event => {
+        this.setState({
+            text : event.target.value,
+        })
+    }
+
+
     render() {
         return (
             <div className={styles.container}>
                 <p>Search what you want</p>
-                <input placeholder="Search ..." type="text" />
+                <div>
+                    <input value={this.state.text} onChange={this.changeHandler} placeholder="Search ..." type="text" />
+                    <br/>
+                    <br/>
+                    <span>{this.state.text}</span>
+                </div>
             </div>
         );
     }
